46fb0dfcfedca5eb8c6d4f40ece1466457608244
Dagbedji Fagnisse <dfagnisse@gmail.com>
2013-09-23 23:19:32 -0400

    setup project. Xcode issues seems to be fixed.

---
+++ CS408-WarmupApp/CS408-WarmupApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CS408-WarmupApp
+//
+//  Created by Dagbedji Fagnisse on 9/23/13.
+//  Copyright (c) 2013 Dagbedji Fagnisse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
